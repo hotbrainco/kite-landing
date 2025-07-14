ade3ab27660c9763382b6f743bd7529bc2ed065a
Drew C <drew@hotbrain.co>
2025-07-14 14:41:31 -0700

    Fix Stripe API key variable name

--- netlify/functions/create-checkout.cjs
+++ netlify/functions/create-checkout.cjs
@@ -1,5 +1,5 @@
 // Load Stripe with secret key from environment variable
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
 
 exports.handler = async (event) => {
   try {
