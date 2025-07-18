88c18192d1cea70fcc892bab628cabea9da32f89
Drew C <drew@hotbrain.co>
2025-07-18 11:04:10 -0700

    Delete extra env.js file causing deployment error

--- src/_data/env.js
+++
@@ -1,7 +0,0 @@
-// src/_data/env.js
-// Expose environment variables to the Eleventy build
-require('dotenv').config();
-
-module.exports = {
-  stripePublishableKey: process.env.STRIPE_API_KEY_PUBLIC
-};
