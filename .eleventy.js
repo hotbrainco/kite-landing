d62b7cc5979bcf604cffa11fe9d3f08f4841e0ef
Drew C <drew@hotbrain.co>
2025-08-11 13:14:41 -0700

    fix: ensure src/css is copied to output for checkout panel styling

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ export default function(eleventyConfig) {
   // Copy static assets
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/styles.css");
   eleventyConfig.addPassthroughCopy("src/script.js");
   eleventyConfig.addPassthroughCopy("src/logo.png");
